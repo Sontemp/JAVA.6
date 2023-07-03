d63c34bb7ab0163a1dadabe795495d41b701cf51
Alex <aleksti8@list.ru>
2023-07-03 13:42:06 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import ru.netology.services.javaqamvn.services.RecreationServices;
+
+public class Main {
+    public static void main(String[] args) {
+        RecreationServices services = new RecreationServices();
+    }
+}
---
+++ src/test/java/ru/netology/services/javaqamvn/services/RecreationServicesTest.java
@@ -0,0 +1,22 @@
+package ru.netology.services.javaqamvn.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RecreationServicesTest {
+
+    @Test
+    public void test(){
+        RecreationServices services = new RecreationServices();
+
+        int income = 10000;
+        int expense = 3000;
+        int threshold = 20000;
+
+        int actual = services.calculate(income, expense, threshold);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
